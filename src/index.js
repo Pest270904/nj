067947b5fa45b985013170c8eb60acb9b70b2528
Pest270904 <ntpllc2004@gmail.com>
2024-02-29 22:39:55 +0700

    hbs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path')
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
 
-app.use(morgan('combined'))
+// HTTP logger
+app.use(morgan("combined"));
 
-app.get('/test', (req, res) => {
-  res.send("Hello world")
-})
+// Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set("view engine", "hbs");
+app.set('views', path.join(__dirname,'resources/views'))
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/page1", (req, res) => {
+  res.render("page1");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
